a64fb1902cd8ec5adbc15b46806af2c0bb0ec254
hugo.asaga@triggo.ai <hugo.asaga@triggo.ai>
2022-04-28 21:01:57 +0000

    fixed typing mistake

--- models/staging/traffic/stg_daily_traffic_events.sql
+++ models/staging/traffic/stg_daily_traffic_events.sql
@@ -3,7 +3,7 @@ with daily_traffic_events as (
         city,
         count(1)  as traffic_events,
         date_trunc('day', "StartTime(UTC)") as day
-    from from {{source ('traffic_events', 'US_TRAFFIC_EVENTS_RAW')}}
+    from {{source ('traffic_events', 'US_TRAFFIC_EVENTS_RAW')}}
     group by 1, 3
     order by 3, 1
 )
