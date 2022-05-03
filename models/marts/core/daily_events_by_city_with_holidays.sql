74484a3cc223366e30328eb946f86f5fa775b5d3
hugo.asaga@triggo.ai <hugo.asaga@triggo.ai>
2022-05-03 15:31:22 +0000

    Added seeds and seeds use case

---
+++ models/marts/core/daily_events_by_city_with_holidays.sql
@@ -0,0 +1,20 @@
+with daily_traffic_events as (
+    select * from {{ref('stg_daily_traffic_events')}}
+),
+
+holidays as (
+select * from {{ref ('US_Holiday_Dates_2004_2021')}}
+),
+
+final as(
+select 
+    daily_traffic_events.city as city,
+    daily_traffic_events.traffic_events,
+    holidays.holiday,
+    daily_traffic_events.day
+    from daily_traffic_events
+    left join holidays
+    ON daily_traffic_events.day=holidays.date
+)
+
+select * from final
--- models/staging/weather/stg_daily_weather_events.sql
+++ models/staging/weather/stg_daily_weather_events.sql
@@ -3,7 +3,7 @@ with daily_weather_events as (
         city,
         count(1)  weather_events,
         date_trunc('day', "StartTime(UTC)") as day
-    rom {{source ('weather_events', 'US_WEATHER_EVENTS_RAW')}}
+    from {{source ('weather_events', 'US_WEATHER_EVENTS_RAW')}}
     where city is not null
     group by city, day
     order by day, city
