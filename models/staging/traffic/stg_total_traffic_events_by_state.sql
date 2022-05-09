038a4e7a3e3813d072a3071db396d519ad58ad45
hugo.asaga@triggo.ai <hugo.asaga@triggo.ai>
2022-05-09 12:54:19 +0000

    events by state

---
+++ models/staging/traffic/stg_total_traffic_events_by_state.sql
@@ -0,0 +1,10 @@
+with total_traffic_events_by_state as (
+    select
+        state,
+        count(1)  traffic_events
+    from {{source ('traffic_events', 'US_TRAFFIC_EVENTS_RAW')}}
+    where state is not null
+    group by state
+)
+
+select * from total_traffic_events_by_state 
